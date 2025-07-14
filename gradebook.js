f6ea1423bb558ca7af5399b49473fdda3693ff6e
gabrielarivdel <gnriveradelgado@my.waketech.edu>
2025-07-13 20:39:31 -0400

    gradebook

---
+++ gradebook.js
@@ -0,0 +1,13 @@
+function fetchGradeData() {
+    console.log("FetchGradeData...");
+}
+
+function populateGradebook(data) {
+    console.log("Populating gradebook with data:", data);
+}
+
+// todo remove this
+// call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook();
+// END REMOVEj
